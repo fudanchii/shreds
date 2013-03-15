1c4f8dfe6dd7a559bdda43c9768d474a3bdae5cf
Nurahmadie <nurahmadie@gmail.com>
2013-03-15 09:33:48 +0700

    Make-up feeds model

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,9 +1,8 @@
 class Feed < ActiveRecord::Base
-  store :meta, accessors: [:last_item_title, :last_item_date]
-  attr_accessible :title, :permalink, :meta
+  store :meta, accessors: [:last_read_title, :last_read_date]
+  attr_accessible :title, :permalink, :meta, \
+                  :last_read_title, :last_read_date
 
   validates :title, presence: true
   validates :permalink, presence: true
-  validates :last_item_title, presence: true
-  validates :last_item_date, presence: true
 end
--- db/migrate/20130314095131_create_feeds.rb
+++ db/migrate/20130314095131_create_feeds.rb
@@ -1,6 +1,9 @@
 class CreateFeeds < ActiveRecord::Migration
   def change
     create_table :feeds do |t|
+      t.string :title, :null => false
+      t.string :permalink, :null => false
+      t.text :meta
 
       t.timestamps
     end
