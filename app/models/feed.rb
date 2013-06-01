a0d1701ba5dac2959bcc734da687eef76c9070f6
Nurahmadie <nurahmadie@gmail.com>
2013-06-01 21:44:31 +0700

    No continuations

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -2,12 +2,14 @@ require 'uri'
 
 class Feed < ActiveRecord::Base
   store :meta, accessors: [:title]
-  attr_accessible :permalink, :meta, :title, \
+  attr_accessible :permalink, :meta, :title
 
   belongs_to :category
   has_many :newsitems
 
   validates :permalink, presence: true
+
+  before_create :check_category
 
   def favicon
     url = URI.parse(self.permalink)
@@ -16,5 +18,11 @@ class Feed < ActiveRecord::Base
 
   def unread_count()
     newsitems.where(unread: 1).count
+  end
+
+  def check_category
+    if category.nil?
+      self.category_id = Category.where(name: "default").first_or_create.id
+    end
   end
 end
