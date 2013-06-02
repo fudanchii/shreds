e9cc820c0897a7e5c9ba69ad01593d0aeee0d357
Nurahmadie <nurahmadie@gmail.com>
2013-06-02 16:05:54 +0700

    parse and save feed directly to database

--- app/workers/feeds_worker.rb
+++ app/workers/feeds_worker.rb
@@ -1,10 +1,23 @@
 require 'feedzirra'
+
 class FeedWorker
   include Sidekiq::Worker
 
   def perform(feed_id)
     feed_record = Feed.find(feed_id)
-    xml = Feedzirra::Feed.fetch_raw(feed_record.permalink)
-    feed_record.update_cache(xml)
+    feed = Feedzirra::Feed.fetch_and_parse(feed_record.url)
+    feed.sanitize_entries!
+    feed_record.title = feed.title
+    feed.entries.each do | entry |
+      item = Newsitem.new({
+        title: entry.title,
+        content: entry.content,
+        permalink: entry.url,
+        author: entry.author,
+        published: entry.published
+      })
+      feed_record.newsitems << item
+      item.save && feed_record.save
+    end
   end
 end
