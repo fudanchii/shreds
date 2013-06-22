0586bf012900c256a7c129079d14a9267401b44b
Nurahmadie <nurahmadie@gmail.com>
2013-06-22 14:33:41 +0700

    Refactor route, add newsitem#show, and stub for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Shreds::Application.routes.draw do
-  resources :feeds, :only => [:create, :show, :destroy], :path => '/'
-  root :to => 'feeds#index'
+  resources :feeds, only: [:index, :create, :show, :destroy], path: '/', format: false do
+    resources :newsitems, only: [:show], path: '/'
+  end
+
+  scope '/api/v1', format: true, constraints: { format: 'json' } do
+    resources :feeds, only: [:index, :create, :show, :destroy] do
+      put 'mark_as_read', on: :member
+      put 'mark_all_as_read', on: :collection
+      resources :newsitems, only: [:show], path: '/' do
+        put 'mark_as_read', on: :member
+      end
+    end
+  end
 end
