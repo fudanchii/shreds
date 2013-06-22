a2356a863332c2a87c26df6a647375e0a5e9a597
Nurahmadie <nurahmadie@gmail.com>
2013-06-22 12:32:10 +0700

    Don't fetch if feeds is up-to-date

--- app/workers/feed_worker.rb
+++ app/workers/feed_worker.rb
@@ -3,23 +3,30 @@ require 'feedzirra'
 class FeedWorker
   include Sidekiq::Worker
 
+  def up_to_date?(feed, feed_record)
+    (not feed_record.etag.nil?)
+    and (feed.etag == feed_record.etag)
+    and not feed_record.newsitems.empty?
+  end
+
   def perform(feed_id)
     feed_record = Feed.find(feed_id)
     feed = Feedzirra::Feed.fetch_and_parse(feed_record.url)
-    if feed.etag == feed_record.etag then
-      return
-    end
+    return if up_to_date?(feed, feed_record)
+    # TODO: Use config to select 
+    # which field should be sanitized
     #feed.sanitize_entries!
     feed_record.title = feed.title
     feed_record.etag = feed.etag
     feed.entries.each do | entry |
       item = Newsitem.where({
         title: entry.title,
-        content: entry.content,
         permalink: entry.url,
-        author: entry.author,
         published: entry.published
       }).first_or_create
+      item.content = entry.content
+      item.author  = entry.author
+      item.summary = entry.summary
       feed_record.newsitems << item
       item.save && feed_record.save
     end
