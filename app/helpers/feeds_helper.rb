79d9eeb70beb759719fef3c781dcf6b09ea5e8f1
Nurahmadie <nurahmadie@gmail.com>
2013-06-23 20:56:48 +0700

    Filter out feeds with 0 unread count

--- app/helpers/feeds_helper.rb
+++ app/helpers/feeds_helper.rb
@@ -3,4 +3,10 @@ module FeedsHelper
     feeds.nil? or feeds.empty? or
     (Feed.total_unread(feeds) == 0)
   end
+
+  def filter_read(feeds)
+    feeds.select do |feed|
+      feed.unread_count > 0
+    end
+  end
 end
