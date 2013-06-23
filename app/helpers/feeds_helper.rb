c3e9969aa763c476fdaf3032a8f63ccf9d0d9384
Nurahmadie <nurahmadie@gmail.com>
2013-06-23 08:55:02 +0700

    Remove feed helpers

--- app/helpers/feeds_helper.rb
+++ app/helpers/feeds_helper.rb
@@ -1,9 +1,2 @@
 module FeedsHelper
-  def content(newsitem)
-    newsitem.content.nil? ? "" : newsitem.content
-  end
-
-  def summary(newsitem)
-    newsitem.summary.nil? ? "" : newsitem.summary
-  end
 end
