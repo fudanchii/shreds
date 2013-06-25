8cd566857b70d2a51fc65cb3b6fb3d48d3d315b2
Nurahmadie <nurahmadie@gmail.com>
2013-06-26 01:45:24 +0700

    Move API url to `/i'

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Shreds::Application.routes.draw do
     resources :newsitems, only: [:show], path: '/'
   end
 
-  scope '/api/v1', format: true, constraints: { format: 'json' } do
+  scope '/i', format: true, constraints: { format: 'json' } do
     resources :feeds, only: [:index, :create, :show, :destroy] do
       put 'mark_as_read', on: :member
       put 'mark_all_as_read', on: :collection
