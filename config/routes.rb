55c0b793d0fed2bc9e4cb50754953fb030a608cf
Nurahmadie <nurahmadie@gmail.com>
2013-06-27 17:54:20 +0700

    Use patch as per rails 4

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,10 @@ Shreds::Application.routes.draw do
 
   scope '/i', format: true, constraints: { format: 'json' } do
     resources :feeds, only: [:index, :create, :show, :destroy] do
-      put 'mark_as_read', on: :member
-      put 'mark_all_as_read', on: :collection
+      patch 'mark_as_read', on: :member
+      patch 'mark_all_as_read', on: :collection
       resources :newsitems, only: [:show], path: '/' do
-        put 'mark_as_read', on: :member
+        patch 'mark_as_read', on: :member
       end
     end
   end
