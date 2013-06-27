093c1554562ad88b596374244f9601dc4e687d25
Nurahmadie <nurahmadie@gmail.com>
2013-06-28 06:32:46 +0700

    Rails's way of building relationship

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_action do
-    @category = Category.new
     @new_feed = Feed.new
-    @new_feed.category = @category
+    @new_feed.category.build
     # XXX: Remove this later
     @categories = Category.all
   end
