ef88174bc490fe97d98b53632f71f3dd2e214f49
Nurahmadie <nurahmadie@gmail.com>
2013-06-29 10:09:57 +0700

    Accidentally create empty category :S

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_action do
-    catname = params[:category] ? params[:category][:name] : ""
-    @category = Category.where(name: catname).first_or_create
+    @category = Category.new
     @new_feed = @category.feeds.build
     # XXX: Remove this later
     @categories = Category.all
