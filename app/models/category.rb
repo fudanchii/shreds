b43c578451497400d61e0072ec121cd27354fc81
Nurahmadie <nurahmadie@gmail.com>
2013-06-29 10:10:49 +0700

    Add safe_destroy method for Category.
    Automatically move Feeds to uncategorized when
    its category removed

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,13 @@
 class Category < ActiveRecord::Base
   has_many :feeds
+
+  def safe_destroy
+    Feed.transaction do
+      feeds.each do |feed|
+        feed.category = Category.where(name: "uncategorized").first
+        feed.save
+      end
+      destroy
+    end
+  end
 end
