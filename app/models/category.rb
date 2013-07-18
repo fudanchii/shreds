0efd7f23ff6b8fd9e42361a99dd5c5228a0adb69
Nurahmadie <nurahmadie@gmail.com>
2013-07-19 02:22:13 +0700

    Add helper methods for Category.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,12 +1,18 @@
 class Category < ActiveRecord::Base
   has_many :feeds
 
+  def self.default
+    "uncategorized"
+  end
+
+  def is_custom_and_unused?
+    feeds.count == 0 and name != self.class.default
+  end
+
   def safe_destroy
+    defcat = Category.where(name: self.class.default).first
     Feed.transaction do
-      feeds.each do |feed|
-        feed.category = Category.where(name: "uncategorized").first
-        feed.save
-      end
+      feeds.each { |feed| feed.update(category: defcat) }
       destroy
     end
   end
