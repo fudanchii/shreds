e2832f65167188760217424c1fcacfbfa79fc98f
Nurahmadie <nurahmadie@gmail.com>
2013-07-19 06:48:30 +0700

    Add tests for internal API

--- test/functional/feeds_controller_test.rb
+++ test/functional/feeds_controller_test.rb
@@ -1,13 +1,9 @@
 require 'test_helper'
 
 describe FeedsController do
-  setup do
-    @feed = feeds(:one)
-  end
+  setup { @feed = feeds(:one) }
 
-  teardown do
-    `redis-cli flushall`
-  end
+  teardown { `redis-cli flushall` }
 
   it "should get index" do
     get :index
@@ -17,13 +13,14 @@ describe FeedsController do
 
   it "should create feed" do
     assert_difference('Feed.count') do
-      post :create, feed: { url: "http://fudanchii.net/atom.xml" }, category: { feed: "uncategorized" }
+      post :create, feed: { url: "http://fudanchii.net/atom.xml" }, category: { feed: Category.default }
     end
     assert_redirected_to feed_path(assigns(:feed))
   end
 
   it "should show feed" do
     get :show, id: @feed
+    assert_not_nil assigns(:feed)
     assert_response :success
   end
 
@@ -32,5 +29,31 @@ describe FeedsController do
       delete :destroy, id: @feed
     end
     assert_redirected_to feeds_path
+  end
+
+  describe "Internal API" do
+    it "should get index (GET /i/feeds.json)" do
+      get :index, format: "json"
+      assert_response :success
+      res = JSON.parse(response.body)
+      res.must_be_instance_of Array
+    end
+
+    it "should create feed (POST /i/feeds.json)" do
+      obj = { url: "http://fudanchii.net/atom.xml" }
+      post :create, feed: obj, category: { feed: Category.default }, format: "json"
+      assert_response :success
+      res = JSON.parse(response.body)
+      res['id'].must_be :>, 0
+      res['url'].must_equal obj[:url]
+    end
+
+    it "should show feed (GET /i/feeds/:id.json)" do
+      get :show, id: @feed, format: "json"
+      assert_response :success
+      res = JSON.parse(response.body)
+      res['url'].must_equal @feed.url
+      res['newsitems'].must_be_instance_of Array
+    end
   end
 end
