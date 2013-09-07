554e6b97683e35fd5819a075487d03c94ddb1efa
Nurahmadie <nurahmadie@gmail.com>
2013-09-07 16:44:19 +0700

    Refactor navbar layouting

--- config/initializers/moji.rb
+++ config/initializers/moji.rb
@@ -8,13 +8,14 @@ module ActionView
   module Helpers
     module TextHelper
       def truncate_moji(str, options = {}, &block)
+        str ||= ''
         length = options[:length] || LEN
         # FIXME: Better ways to balance fullwidth and halfwidth strings
         #        truncation.
         # Check str for fullwidth characters then set length equal to
         # desired options[:length] halved, plus half of the number of its
         # halfwidth characters. Or return the full length if there's none.
-        options[:length] = Moji.type?(str || '', Moji::ZEN) ?
+        options[:length] = Moji.type?(str, Moji::ZEN) ?
           length/2 + str[0,length/2].scan(Moji.regexp(Moji::HAN)).length/2 + 2:
           length
         truncate(str, options, &block)
