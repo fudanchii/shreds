3b99f3fb472c3ff376109dc399be6d3a4f480ae8
Nurahmadie <nurahmadie@gmail.com>
2013-09-15 13:55:04 +0700

    Use feedbag, swap `feed_url` with `url` and add link to canonical url.

--- app/workers/feed_worker.rb
+++ app/workers/feed_worker.rb
@@ -1,4 +1,5 @@
 require 'feedzirra'
+require 'feedbag'
 
 class FeedWorker
   include Sidekiq::Worker
@@ -25,18 +26,22 @@ class FeedWorker
 
   def fetch(feed_id)
     feed_record = Feed.find(feed_id)
-    feed = Feedzirra::Feed.fetch_and_parse(feed_record.url)
+    feed_record.feed_url ||= Feedbag.find(feed_record.url).first
+    feed_record.destroy && return if feed_record.feed_url.nil?
+    feed = Feedzirra::Feed.fetch_and_parse(feed_record.feed_url)
     return if up_to_date?(feed, feed_record)
+
     # TODO: Use config to select which field should be sanitized
     #feed.sanitize_entries!
-    feed_record.update(title: feed.title, etag: feed.etag)
+
+    feed_record.update(title: feed.title, etag: feed.etag, url: feed.url)
     feed.entries.each do | entry |
       item = Newsitem.where({
         permalink: entry.url,
       }).first
       if item.nil?
         item = feed_record.newsitems.build(newsitem_params(entry))
-        item.save && feed_record.save
+        item.save
       end
     end
   end
