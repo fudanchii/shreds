c1a5f5c39c026eda2b55a64e2d31ca77f42cfe4c
Nurahmadie <nurahmadie@gmail.com>
2013-09-20 02:19:07 +0700

    Schedule delete already read newsitem each day at 2 o'clock.

---
+++ app/workers/feed_clear_read_worker.rb
@@ -0,0 +1,12 @@
+class FeedClearReadWorker
+  include Sidekiq::Worker
+  include Sidetiq::Schedulable
+
+  sidekiq_options :retry => false
+
+  recurrence { daily.hour_of_day(2) }
+
+  def perform
+    Feed.all.each { |f| f.clear_read_news(1.day.ago) }
+  end
+end
