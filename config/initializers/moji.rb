f290a2e549e53976ec749095718778c91a63920c
Nurahmadie <nurahmadie@gmail.com>
2013-09-19 14:01:56 +0700

    Better title truncation...?

--- config/initializers/moji.rb
+++ config/initializers/moji.rb
@@ -3,6 +3,13 @@ require 'moji'
 # http://api.rubyonrails.org/classes/ActionView/Helpers/TextHelper.html#method-i-truncate
 # :length defaults to 30
 LEN = 30
+OMMISION = '...'
+
+def deter_ommision(str, hanlength, olength)
+  han_count = str[0, hanlength].scan(Moji.regexp(Moji::HAN)).length
+  Moji.type?(str[-olength, olength], Moji::ZEN)
+  han_count/2
+end
 
 module ActionView
   module Helpers
@@ -10,13 +17,15 @@ module ActionView
       def truncate_moji(str, options = {}, &block)
         str ||= ''
         length = options[:length] || LEN
+        ommision = options[:ommision] || OMMISION
+        olength = ommision.each_char.reduce(0) {|h,c| Moji.type?(c, Moji::ZEN) ? h+2 : h+1 }
         # FIXME: Better ways to balance fullwidth and halfwidth strings
         #        truncation.
         # Check str for fullwidth characters then set length equal to
         # desired options[:length] halved, plus half of the number of its
         # halfwidth characters. Or return the full length if there's none.
-        options[:length] = Moji.type?(str, Moji::ZEN) ?
-          length/2 + str[0,length/2].scan(Moji.regexp(Moji::HAN)).length/2 + 2:
+        options[:length] = Moji.type?(str || '', Moji::ZEN) ?
+          length/2 + deter_ommision(str, length/2, olength) :
           length
         truncate(str, options, &block)
       end
