6ec00204f127c2507c54264c12a7f5009e30b6e3
Nurahmadie <nurahmadie@gmail.com>
2013-10-07 01:08:03 +0700

    Refactor events#watch

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,17 +6,12 @@ class EventsController < ApplicationController
       unless result.empty? then
         idx = 0
         # XXX: Need proper atomic redis get-then-delete, i.e.
-        # Avoid racing condition where key is just created when delete being called.
+        # Avoid racing condition where key was just created when delete being called.
         watchList.each {|w| $redis.del(w) unless result[idx].nil?; idx += 1 }
         idx, rmap = 0, {}
         result.each {|r| rmap[watchList[idx]] = JSON.parse(r) unless r.nil?; idx += 1 }
-        if rmap.empty? then
-          render :text => ''
-        else
-          render :json => rmap
-        end
+        return render(:json => rmap) unless rmap.empty?
       end
-      return
     end
     render :text => ''
   end
