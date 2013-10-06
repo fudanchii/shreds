afa0c2585e261cf14595e8539ded76c5d8ea7abd
Nurahmadie <nurahmadie@gmail.com>
2013-10-07 03:22:20 +0700

    Refactor notification, put html as global.

--- app/assets/javascripts/notification.js
+++ app/assets/javascripts/notification.js
@@ -1,10 +1,11 @@
 $(function () {
   var $doc = $(document);
   var $container = $('.alert-container');
+  var notif = '<div class="alert fade alert-float"/>';
+  var close = '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>';
 
   $doc.on('shreds:notification:error', function (ev, message) {
-    var $notif = $('<div class="alert alert-danger fade alert-float"/>');
-    var close = '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>';
+    var $notif = $(notif).addClass('alert-danger');
     $notif.html(close + message);
     $container.append($notif);
     setTimeout(function () { $notif.addClass('in'); }, 100);
