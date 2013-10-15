8b1b28c82dc982fbb8969503d07e778e16057d7c
Nurahmadie <nurahmadie@gmail.com>
2013-10-16 02:36:52 +0700

    Refactor watch, use recursive call when promise failed.

--- app/assets/javascripts/watch.js
+++ app/assets/javascripts/watch.js
@@ -5,21 +5,16 @@
     list: [],
     add: function (data) {
       this.list.push(data);
-      doWatch.call(this).fail(function () {
-        var eventWatch = setInterval(function () {
-          doWatch.call(this);
-          if (this.list.length === 0) {
-            clearInterval(eventWatch);
-          }
-        }.bind(this), 2000);
-      }.bind(this));
+      setTimeout(doWatch.bind(this), 1000);
     },
     init: function (ctx) {
-      setInterval(doWatch.bind(this), 600000);
+      setInterval(doWatch.bind(this, 'updateFeed'), 600000);
     }
   };
 
-function doWatch() {
+function doWatch(key) {
+  if (key) { this.list.push(key); }
+  if (this.list.length === 0) { return; }
   return $.ajax('/i/watch.json', {
     data: { 'watchList': this.list.join() }
   }).done(function (data) {
@@ -30,6 +25,8 @@ function doWatch() {
     this.list = this.list.filter(function (el) {
       return !data[el];
     });
+  }.bind(this)).fail(function () {
+    setTimeout(doWatch.bind(this), 2000);
   }.bind(this));
 }
 
