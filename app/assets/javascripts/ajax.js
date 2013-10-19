f414621e905c9179d44d45e20134cc1fc008f124
Nurahmadie <nurahmadie@gmail.com>
2013-10-19 09:35:26 +0700

    Adopt auto `X-CSRF-Token` header addition from jQuery_ujs

--- app/assets/javascripts/ajax.js
+++ app/assets/javascripts/ajax.js
@@ -3,45 +3,37 @@
   Shreds.components.push(name);
   Shreds[name] = {
     init: function () {
+      $.ajaxPrefilter(function (opts, oriOpts, xhr) {
+        if (!opts.crossDomain) {
+          var token = $('meta[name="csrf-token"]').attr('content');
+          if (token) { xhr.setRequestHeader('X-CSRF-Token', token); }
+        }
+      });
     },
     'do': function (url, opts) {
-      var ajax = $.ajax(url, opts.ajax);
+      var ajax = $.ajax(url, opts.opts);
       if (opts.doWatch) {
-        ajax.done(function (data) {
-          if (data && data.watch) {
-            Shreds.watch.add(data.watch);
-          }
-        });
+        ajax.done(function (data) { if (data && data.watch) { Shreds.watch.add(data.watch); } });
       }
       if (opts.failMsg) {
-        ajax.fail(function () {
-          Shreds.notification.error(opts.failMsg);
-        })
+        ajax.fail(function () { Shreds.notification.error(arguments[2] || opts.failMsg); });
       }
       return ajax;
     },
     get: function (url, opts) {
-      var options = $.extend({}, opts, {
-        ajax: { type: 'GET' }
-      });
+      var options = $.extend(true, {}, opts, { opts: { type: 'GET' } });
       return this.do(url, options);
     },
     post: function (url, opts) {
-      var options = $.extend({}, opts, {
-        ajax: { type: 'POST' }
-      });
+      var options = $.extend(true, {}, opts, { opts: { type: 'POST' } });
       return this.do(url, options);
     },
     patch: function (url, opts) {
-      var options = $.extend({}, opts, {
-        ajax: { type: 'PATCH' }
-      });
+      var options = $.extend(true, {}, opts, { opts: { type: 'PATCH' } });
       return this.do(url, options);
     },
     'delete': function (url, opts) {
-      var options = $.extend({}, opts, {
-        ajax: { type: 'DELETE' }
-      });
+      var options = $.extend(true, {}, opts, { opts: { type: 'DELETE' } });
       return this.do(url, options);
     }
   };
