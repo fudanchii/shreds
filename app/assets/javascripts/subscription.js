7dcc017909b8a92dc02ffeeccf370dee16ef4b38
Nurahmadie <nurahmadie@gmail.com>
2013-10-27 11:14:14 +0700

    Relayout fileupload source

--- app/assets/javascripts/subscription.js
+++ app/assets/javascripts/subscription.js
@@ -15,20 +15,20 @@
   function setupFileUpload() {
     var spinner = Ladda.create($('.fileinput-button')[0]);
 
-    $('#fileupload').fileupload({
-      dataType: 'json'
-    }).on('fileuploadstart', function () {
-      spinner.start();
-    }).on('fileuploaddone', function (e, data) {
-      var result = data.result;
-      if (result && result.error) {
-        Shreds.notification.error(result.error);
+    $('#fileupload').fileupload({ dataType: 'json' })
+      .on('fileuploadstart', function () { spinner.start(); })
+      .on('fileuploaddone', function (e, data) {
+        var result = data.result;
+        if (result && result.error) {
+          Shreds.notification.error(result.error);
+          spinner.stop();
+        } else if (result && result.watch) { Shreds.watch.add(result.watch); }
+      })
+      .on('fileuploadfail', function () {
+        Shreds.notification.error('<strong>Can\'t</strong> upload file.');
         spinner.stop();
-      } else if (result && result.watch) { Shreds.watch.add(result.watch); }
-    }).on('fileuploadfail', function () {
-      Shreds.notification.error('<strong>Can\'t</strong> upload file.');
-      spinner.stop();
-    }).on('spinnerstop', function () { spinner.stop(); });
+      })
+      .on('spinnerstop', function () { spinner.stop(); });
   }
 })(window.Shreds);
 
