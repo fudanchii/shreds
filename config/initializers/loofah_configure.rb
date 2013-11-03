2508a8066022ed228fa6daed7b218b42cb451183
Nurahmadie <nurahmadie@gmail.com>
2013-11-04 02:42:31 +0700

    Refactor loofah's patch

--- config/initializers/loofah_configure.rb
+++ config/initializers/loofah_configure.rb
@@ -5,11 +5,9 @@
 #
 # see https://github.com/flavorjones/loofah/blob/master/lib/loofah/html5/whitelist.rb
 # for current default whitelist.
-begin
+if ENV["WLTAGS"]
   tag_list = Loofah::HTML5::WhiteList::ALLOWED_ELEMENTS_WITH_LIBXML2
-  tag_list += ENV["WLTAGS"].split if ENV["WLTAGS"]
+  tag_list += ENV["WLTAGS"].split
   Loofah::HTML5::WhiteList::send :remove_const, :ALLOWED_ELEMENTS_WITH_LIBXML2
   Loofah::HTML5::WhiteList::const_set :ALLOWED_ELEMENTS_WITH_LIBXML2, tag_list
-rescue => ex
-  logger.warn "Monkey-patching Loofah failed: #{ex.message}"
 end
