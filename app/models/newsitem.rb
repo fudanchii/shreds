c9f9b8e0fac2c665b815f7932704266d2a7a68db
Nurahmadie <nurahmadie@gmail.com>
2013-11-07 01:57:19 +0700

    Fix query for next/prev newsitems link.

--- app/models/newsitem.rb
+++ app/models/newsitem.rb
@@ -1,19 +1,19 @@
 class Newsitem < ActiveRecord::Base
   belongs_to :feed, touch: true
-  scope :for_view, -> { order('published DESC') }
+  scope :for_view, -> { order('published DESC').order('id DESC') }
   before_destroy { Itemhash.insert(permalink) unless unread }
 
   def next
-    adj('published <= ?').first
+    adj('(published < :pdate and id <> :id) or (published = :pdate and id < :id)').first
   end
 
   def prev
-    adj('published >= ?').last
+    adj('(published > :pdate and id <> :id) or (published = :pdate and id > :id)').last
   end
 
   private
 
   def adj(comp)
-    Newsitem.for_view.where(:feed_id => feed_id).where(comp, published).where.not(:id => id)
+    Newsitem.for_view.where(:feed_id => feed_id).where(comp, { :pdate => published, :id => id})
   end
 end
