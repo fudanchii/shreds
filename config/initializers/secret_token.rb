484a797b240958b692882afe40b5a8f2a66ca7e9
Nurahmadie <nurahmadie@gmail.com>
2013-11-07 02:55:22 +0700

    Update rails base structures. `rake rails:update`

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,10 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
 
-fail "Please setup secret token to use with rails, you may want to use `rake secret`" if ENV['SECRET'].nil?
-Shreds::Application.config.secret_token = ENV['SECRET']
-Shreds::Application.config.secret_key_base = ENV['SECRET']
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shreds::Application.config.secret_key_base = 'fc18f5027cd52f8c25bc1843b21cb49099be7214d0fdaf305f36e159ce0f329ba360611a5cc25bc839eec8f1cf7003d5afdb54b73f2d14fe530d6a97ca4a87f9'
