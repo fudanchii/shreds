38f64c347e8767bd5e26fe8c4e6923614d99457d
Nurahmadie <nurahmadie@gmail.com>
2013-11-09 18:25:03 +0700

    Scope feed at events watch.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,7 +10,7 @@ class EventsController < ApplicationController
         watch_list.zip(result).each do |w, r|
           next if r.nil?
           data = JSON.parse r
-          data['feed'] = Feed.find data['id'] if data['id']
+          data['feed'] = Feed.with_unread_count.find data['id'] if data['id']
           data['category'] = Category.find data['category_id'] if data['category_id']
           @payload[w] = data
           EventPool.remove w
