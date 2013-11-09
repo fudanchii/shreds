b0bd26d33158c013ed91df5623b386644d3536c0
Nurahmadie <nurahmadie@gmail.com>
2013-11-09 18:58:45 +0700

    Refactor contexts, use private method.

--- app/contexts/entry_newsitems_context.rb
+++ app/contexts/entry_newsitems_context.rb
@@ -1,19 +1,14 @@
 class EntryNewsitemsContext < BaseContext
   class_attribute :feed, :feed_record
 
+  at_execution do
+    process_entries
+  end
+
   def initialize(feed, feed_record)
     fail if feed.is_a? Fixnum # Got HTTP response code instead of Feed object ^^;
     fail if up_to_date?(feed, feed_record)
     self.feed, self.feed_record = [feed, feed_record]
-  end
-
-  at_execution do
-    feed.sanitize_entries!
-    feed_record.update!(:title => feed.title, :etag => feed.etag, :url => feed.url)
-    feed.entries.each do | entry |
-      next unless Newsitem.where(permalink: entry.url).first.nil? && (not Itemhash.has? entry.url)
-      feed_record.newsitems.build(newsitem_params(entry)).save!
-    end
   end
 
   def up_to_date?(feed, feed_record)
@@ -23,6 +18,15 @@ class EntryNewsitemsContext < BaseContext
   end
 
   private
+
+  def process_entries
+    feed.sanitize_entries!
+    feed_record.update!(:title => feed.title, :etag => feed.etag, :url => feed.url)
+    feed.entries.each do | entry |
+      next unless Newsitem.where(permalink: entry.url).first.nil? && (not Itemhash.has? entry.url)
+      feed_record.newsitems.build(newsitem_params(entry)).save!
+    end
+  end
 
   def newsitem_params(entry)
     ActionController::Parameters.new(
--- app/contexts/feed_creation_context.rb
+++ app/contexts/feed_creation_context.rb
@@ -1,27 +1,29 @@
 class FeedCreationContext < BaseContext
   class_attribute :url, :feed_url, :category, :feed
 
+  at_execution do
+    create_feed
+  end
+
   def initialize(category, url, feed_url)
     self.url, self.feed_url = [url, feed_url]
     self.category = Category.where(:name => category).first_or_create
   end
 
-  at_execution do
-    begin
-      self.feed = category.feeds.build(feed_params(url, feed_url))
-      self.feed.save!
-    rescue => ex
-      category.destroy if category.is_custom_and_unused?
-      fail
-    end
-  end
-
   def and_then
-    yield self.feed if block_given?
+    yield feed if block_given?
     result
   end
 
   private
+
+  def create_feed
+    self.feed = category.feeds.build(feed_params(url, feed_url))
+    self.feed.save!
+  rescue => ex
+    category.destroy if category.is_custom_and_unused?
+    fail
+  end
 
   def feed_params(url, feed_url)
     ActionController::Parameters.new(:url => url, :feed_url => feed_url, :title => url).permit!
