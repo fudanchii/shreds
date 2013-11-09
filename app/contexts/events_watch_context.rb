c5a64bb004d42175b8a86b1043e624285ff943c8
Nurahmadie <nurahmadie@gmail.com>
2013-11-09 18:59:51 +0700

    Contextify events watch.

---
+++ app/contexts/events_watch_context.rb
@@ -0,0 +1,28 @@
+class EventsWatchContext < BaseContext
+  class_attribute :payload, :watch_list
+
+  at_execution do
+    fetch_events
+  end
+
+  def initialize(watch_str)
+    self.watch_list = watch_str.split(',') if watch_str
+    self.payload = {}
+  end
+
+  private
+
+  def fetch_events
+    return if watch_list.empty?
+    result = EventPool.find(*watch_list)
+    return if result.empty?
+    watch_list.zip(result).each do |w, r|
+      next if r.nil?
+      data = JSON.parse r
+      data['feed'] = Feed.with_unread_count.find(data['id'].to_i) if data['id']
+      data['category'] = Category.find(data['category_id'].to_i) if data['category_id']
+      payload[w] = data
+      EventPool.remove w
+    end
+  end
+end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,24 +1,7 @@
 class EventsController < ApplicationController
-
-  before_action { @payload = {} }
-
   def watch
-    watch_list = params[:watchList].split(',') if params[:watchList]
-    unless watch_list.empty?
-      result = EventPool.find(*watch_list)
-      unless result.empty?
-        watch_list.zip(result).each do |w, r|
-          next if r.nil?
-          data = JSON.parse r
-          data['feed'] = Feed.with_unread_count.find data['id'] if data['id']
-          data['category'] = Category.find data['category_id'] if data['category_id']
-          @payload[w] = data
-          EventPool.remove w
-        end
-        return render 'watch' unless @payload.empty?
-      end
-    end
+    @payload = EventsWatchContext.new(params[:watchList]).have_payload.execute
+    return render 'watch' unless @payload.empty?
     render :text => ''
   end
-
 end
