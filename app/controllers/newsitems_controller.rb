851f45923d2f2d17c784498771bc1fa84fdf8cd2
Nurahmadie <nurahmadie@gmail.com>
2013-12-22 11:41:16 +0700

    Explicitly cast newsitem's id to integer.
    Fix error when user input non valid id number.

--- app/controllers/newsitems_controller.rb
+++ app/controllers/newsitems_controller.rb
@@ -3,7 +3,7 @@ class NewsitemsController < ApplicationController
 
   before_action do
     @feed = Feed.includes(:newsitems)
-      .where('feeds.id = ? and newsitems.id = ?', params[:feed_id].to_i, params[:id])
+      .where('feeds.id = ? and newsitems.id = ?', params[:feed_id].to_i, params[:id].to_i)
       .references(:newsitems).to_ary.first
     fail ActiveRecord::RecordNotFound if (@feed.nil? || @feed.newsitems.empty?)
     @newsitem = @feed.newsitems.first
