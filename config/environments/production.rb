b7b45f8552d47ec8312f57b88efddb51881a77de
Nurahmadie <nurahmadie@gmail.com>
2013-12-23 04:31:14 +0700

    Add some whitelist for better_errors. And tweak some configuration.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Shreds::Application.configure do
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -73,7 +73,7 @@ Shreds::Application.configure do
   config.active_support.deprecation = :notify
 
   # Disable automatic flushing of the log to improve performance.
-  # config.autoflush_log = false
+  config.autoflush_log = false
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
