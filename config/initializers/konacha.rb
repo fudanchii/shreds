a953aa0390fe651045b8b3e0f3df86247e90c1b5
Nurahmadie <nurahmadie@gmail.com>
2014-02-06 21:44:17 +0700

    config.verbose is noop.

--- config/initializers/konacha.rb
+++ config/initializers/konacha.rb
@@ -1,6 +1,5 @@
 Konacha.configure do |config|
   require 'capybara/poltergeist'
-  config.verbose      = true
   config.spec_dir     = "test/javascripts"
   config.spec_matcher = /_spec\.|_test\./
   config.stylesheets  = %w(application)
