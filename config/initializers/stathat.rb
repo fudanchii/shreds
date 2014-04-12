2b6b14a1ca3972c3ac00317101395d3c0e6cf4dd
Nurahmadie <nurahmadie@gmail.com>
2014-04-12 13:24:46 +0700

    Refactor instrumentation code

--- config/initializers/stathat.rb
+++ config/initializers/stathat.rb
@@ -3,11 +3,18 @@ if Rails.env.production? && ENV["STAT_ACCOUNT"].present?
     duration = (finish - start) * 1000
     unless payload[:view_runtime].nil?
       view_time = payload[:view_runtime]
-      StatHat::API.ez_post_value("rails request view duration", ENV["STAT_ACCOUNT"], view_time)
+      StatHat::API.ez_post_value("View Rendering", ENV["STAT_ACCOUNT"], view_time)
     end
-    StatHat::API.ez_post_value("rails request duration", ENV["STAT_ACCOUNT"], duration)
+    StatHat::API.ez_post_value("Response Time", ENV["STAT_ACCOUNT"], duration)
     if duration > 500
-      StatHat::API.ez_post_count("rails slow requests", ENV["STAT_ACCOUNT"], 1)
+      StatHat::API.ez_post_count("Slow Requests", ENV["STAT_ACCOUNT"], 1)
     end  
+  end
+
+  ActiveSupport::Notifications.subscribe "sql.active_record" do |name, start, finish, id, payload|
+    if payload[:name] == "SQL"
+      duration = (finish - start) * 1000
+      StatHat::API.ez_post_value("DB Query", ENV["STAT_ACCOUNT"], duration)
+    end
   end
 end
