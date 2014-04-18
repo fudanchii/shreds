6b75b681257f97b0828dab6c15a4fba75e30c1e5
Nurahmadie <nurahmadie@gmail.com>
2014-04-18 23:24:54 +0700

    Add instrumentation logs

--- config/initializers/stathat.rb
+++ config/initializers/stathat.rb
@@ -1,4 +1,5 @@
 if Rails.env.production? && ENV["STAT_ACCOUNT"].present?
+  instLog ||= Logger.new("#{Rails.root}/log/perf.log")
   ActiveSupport::Notifications.subscribe "process_action.action_controller" do |name, start, finish, id, payload|
     duration = (finish - start) * 1000
     unless payload[:view_runtime].nil?
@@ -8,13 +9,19 @@ if Rails.env.production? && ENV["STAT_ACCOUNT"].present?
     StatHat::API.ez_post_value("Response Time", ENV["STAT_ACCOUNT"], duration)
     if duration > 500
       StatHat::API.ez_post_count("Slow Requests", ENV["STAT_ACCOUNT"], 1)
-    end  
+    end
+    if duration > 100
+      instLog.info("[action] #{payload[:method]} #{payload[:path]}.#{payload[:format].to_s} #{duration}ms")
+    end
   end
 
   ActiveSupport::Notifications.subscribe "sql.active_record" do |name, start, finish, id, payload|
-    if payload[:name] == "SQL"
+    if payload[:sql]
       duration = (finish - start) * 1000
       StatHat::API.ez_post_value("DB Query", ENV["STAT_ACCOUNT"], duration)
+      if duration > 50
+        instLog.info("[sql] #{payload[:name]} '#{payload[:sql]}' #{duration}ms")
+      end
     end
   end
 end
