ceb9e40b9588b166dfbf7f7135a1b439606274f2
Nurahmadie <nurahmadie@gmail.com>
2014-07-20 03:36:32 +0700

    put setupFileUpload directly as init function

--- app/assets/javascripts/shreds_components/subscription.js
+++ app/assets/javascripts/shreds_components/subscription.js
@@ -7,26 +7,25 @@
       spinner.start();
     },
     stopSpinner: function () { spinner.stop(); },
-    init: function () { setupFileUpload(); }
-  });
-
-  function setupFileUpload() {
-    var spinner = Ladda.create($('.fileinput-button')[0]);
-
-    $('#fileupload').fileupload({ dataType: 'json' })
-      .on('fileuploadstart', function () { spinner.start(); })
-      .on('fileuploaddone', function (e, data) {
-        var result = data.result;
-        if (result && result.error) {
-          Shreds.notification.error(result.error);
+    init: function () {
+      var spinner = Ladda.create($('.fileinput-button')[0]);
+      $('#fileupload').fileupload({ dataType: 'json' })
+        .on('fileuploadstart', function () { spinner.start(); })
+        .on('fileuploaddone', function (e, data) {
+          var result = data.result;
+          if (result && result.error) {
+            Shreds.notification.error(result.error);
+            spinner.stop();
+          } else if (result && result.watch) {
+            Shreds.watch.add(result.watch);
+          }
+        })
+        .on('fileuploadfail', function () {
+          Shreds.notification.error('<strong>Can\'t</strong> upload file.');
           spinner.stop();
-        } else if (result && result.watch) { Shreds.watch.add(result.watch); }
-      })
-      .on('fileuploadfail', function () {
-        Shreds.notification.error('<strong>Can\'t</strong> upload file.');
-        spinner.stop();
-      })
-      .on('spinnerstop', function () { spinner.stop(); });
-  }
+        })
+        .on('spinnerstop', function () { spinner.stop(); });
+    }
+  });
 })(window.Shreds);
 
