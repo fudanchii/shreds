c2c371afc3d52b0c38cfb13547805894600910bf
Nurahmadie <nurahmadie@gmail.com>
2014-08-24 13:37:03 +0700

    Also order descending by newsitems.id

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -7,7 +7,7 @@ class Subscription < ActiveRecord::Base
 
   before_save :ensure_category
 
-  scope :for_view, -> { includes(:newsitems).order('newsitems.published desc') }
+  scope :for_view, -> { includes(:newsitems).order('newsitems.published desc, newsitems.id desc') }
   scope :with_unread_count, -> {
     joins(:entries)
       .select('subscriptions.*, sum(case when entries.unread then 1 else 0 end) as unreads')
