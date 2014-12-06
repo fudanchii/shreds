1698021eb5f5f83449fba090fca4ed95846cd1d0
Nurahmadie <nurahmadie@gmail.com>
2014-12-06 12:33:33 +0700

    Refactor session controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,8 +2,8 @@ class SessionController < ApplicationController
   skip_before_action :should_authenticate?, only: [:create]
   skip_before_action :fetch_subscriptions, :init_empty_subscription
 
-  skip_before_action :verify_authenticity_token, only: [:create],
-                     if: -> { Rails.env.development? }
+  skip_before_action :verify_authenticity_token,
+                     only: [:create], if: -> { Rails.env.development? }
 
   def create
     user = UserProvider.create(params[:provider]).sign request.env['omniauth.auth']
