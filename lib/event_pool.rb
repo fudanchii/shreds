531fcb24e76572f1e242307d111ebd6b3154d533
Nurahmadie <nurahmadie@gmail.com>
2014-12-17 23:58:06 +0700

    Refactor redis pool

--- lib/event_pool.rb
+++ lib/event_pool.rb
@@ -2,11 +2,10 @@ require 'redis-namespace'
 
 module EventPool
   def init(ns, rconf)
-    connsize = rconf[:connections].to_i
-    timeout = rconf[:timeout].to_i
-    connsize = 10 if connsize == 0
-    timeout = 30 if timeout == 0
-    @pool ||= ConnectionPool.new(size: connsize, timeout: timeout) do
+    rconf[:connections] = 10 if rconf[:connections].to_i == 0
+    rconf[:timeout] = 30 if rconf[:timeout].to_i == 0
+    @pool = ConnectionPool.new(
+            size: rconf[:connections].to_i, timeout: rconf[:timeout].to_i) do
       Redis::Namespace.new ns, rconf
     end
   end
