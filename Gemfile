3c1305966e40a5c5bc147bf2f5c75d90e4ba6d8a
Nurahmadie <nurahmadie@gmail.com>
2014-12-20 13:50:12 +0700

    ~ Rails 4.2 ~

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'rails', '~> 4.1.5'
+gem 'rails', '~> 4.2'
 
 ### 1. Database and modelling related gems
 gem 'pg'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
+
   BetterErrors::Middleware.allow_ip! ENV['trusted_ip'] if ENV.key?('trusted_ip')
 
   # In the development environment your application's code is reloaded on
@@ -26,6 +28,10 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
+  # yet still be able to expire them through the digest params.
+  config.assets.digest = true
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
