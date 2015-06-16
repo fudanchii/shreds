38c83bece086df8fb15feb836cd4b4d8ea450200
Nurahmadie <nurahmadie@gmail.com>
2015-06-17 05:57:59 +0700

    Refactor Shreds::Feed:Filters should parse only once

--- lib/shreds/feed/filters.rb
+++ lib/shreds/feed/filters.rb
@@ -10,37 +10,41 @@ module Shreds
         # Run all filters against this entry,
         # entry is assumed to be an instance of Newsitem model
         def run(entry)
+          @permalink = Shreds::Feed.to_valid_url entry.permalink
           %i(img_src_to_full_url remove_inline_style).each do |method|
-            send method, entry
+            traverse_transform(entry) { |node| send method, node }
           end
         end
 
         # Turn image with relative path to its absolute URL
-        def img_src_to_full_url(entry)
-          permalink = Shreds::Feed.to_valid_url entry.permalink
-          %w(content summary).each do |prop|
-            fragment = Nokogiri::XML::DocumentFragment.parse entry.send(prop)
-            traverse_transform(fragment) do |node|
-              if node.name.eql?('img') && node.attributes['src'] &&
-                 (!node.attributes['src'].value.urlish?)
-                node.attributes['src'].value = URI.join(permalink, node.attributes['src'].value).to_s
-              end
-            end
-            entry.send :"#{prop}=", fragment.to_s
+        def img_src_to_full_url(node)
+          if node.name.eql?('img') && node.attributes['src'] &&
+             (!node.attributes['src'].value.urlish?)
+            node.attributes['src'].value = URI.join(@permalink, node.attributes['src'].value).to_s
           end
         end
 
-        def remove_inline_style(entry)
-          entry
+        def remove_inline_style(node)
+          node
         end
 
         private
 
-        def traverse_transform(fragment, &block)
-          nodes = fragment.children
-          until nodes.empty?
-            nodes.each { |node| yield node }
-            nodes = nodes.children
+        def fragments(entry)
+          @fragments ||= %w(content summary).map do |method|
+            [Nokogiri::XML::DocumentFragment.parse(entry.send(method)),
+             method]
+          end
+        end
+
+        def traverse_transform(entry, &block)
+          fragments(entry).each do |fragment, property|
+            nodes = fragment.children
+            until nodes.empty?
+              nodes.each { |node| yield node }
+              nodes = nodes.children
+            end
+            entry.send :"#{property}=", fragment.to_s
           end
         end
       end
