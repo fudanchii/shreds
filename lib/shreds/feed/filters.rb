876cd8c7215ff6d37c00f52ed709f3f8fb35a80a
Nurahmadie <nurahmadie@gmail.com>
2015-09-12 15:14:30 +0700

    Fix rubocop complains: indent literal array

--- lib/shreds/feed/filters.rb
+++ lib/shreds/feed/filters.rb
@@ -37,7 +37,7 @@ module Shreds
         def fragments(entry)
           @fragments ||= %w(content summary).map do |method|
             [Nokogiri::HTML::DocumentFragment.parse(entry.send(method)),
-            method] if entry.send(method).present?
+             method] if entry.send(method).present?
           end.compact
         end
 
