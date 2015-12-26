12cce5dc223ba5c89052796f75cf13fe2a0f9a36
Nurahmadie <nurahmadie@gmail.com>
2015-12-26 13:02:46 +0700

    Using es6 version here will breaks the whole stuff

--- app/assets/javascripts/main_include.js
+++ app/assets/javascripts/main_include.js
@@ -2,7 +2,7 @@
 //= require i18n/translations
 
 //= require ractive
-//= require ractive-transitions-fade
+//= require ractive-transitions-fade/ractive-transitions-fade.umd
 
 //= require semantic-ui/semantic
 
