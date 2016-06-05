e0388c4c9b69d24612bf75cce00b5c316f571028
Nurahmadie <nurahmadie@gmail.com>
2016-06-06 01:54:28 +0700

    Move newsitem methods to article

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,15 @@ class Article < ActiveRecord::Base
   belongs_to :feed
   has_many :entries
 
+  scope :for_view, -> { order('published DESC, id DESC') }
+  scope :latest_issue, -> { for_view.take 1 }
+
+  before_destroy :hash_permalink
+
+  before_create :filter_content
+
+  before_save { permalink.strip! }
+
   class << self
     def from_subscription_with_unreads(subscription, options)
       subscription.articles.joins(:entries)
@@ -17,6 +26,30 @@ class Article < ActiveRecord::Base
         .where("row_num <= ?", options[:articles_per_feed])
     end
 
+    def sanitize_field(entry)
+      params = {}
+      %i(title published content author summary).each do |field|
+        params[field] = entry.send(field) if entry.respond_to? field
+      end
+      params[:permalink] = get_entry_url entry
+      ActionController::Parameters.new(params).permit!
+    end
+
+    def has?(link)
+      ctlnk = link.dup
+      links = [link.dup]
+      scheme = ctlnk.slice! %r{^https?://}
+      links << case scheme
+               when 'http://'
+                 ctlnk.prepend('https://')
+               when 'https://'
+                 ctlnk.prepend('http://')
+               end
+      links.compact.any? do |lnk|
+        find_by(permalink: lnk).present? || Itemhash.has?(lnk)
+      end
+    end
+
     private
 
     def entries_query(subscriptions)
@@ -30,7 +63,42 @@ class Article < ActiveRecord::Base
           ) as row_num
         SQL
     end
+
+    def get_entry_url(entry)
+      entry.url.presence.strip || (entry.entry_id.strip if entry.entry_id.urlish?)
+    end
   end
+
+  def next
+    compare_by("<").first
+  end
+
+  def prev
+    compare_by(">").last
+  end
+
+  def unreads
+    entries.where(unread: true).count
+  end
+
+  private
+
+  def compare_by(op)
+    op = "<" unless %w(< >).includes? op
+    self.for_view
+      .where(feed_id: feed_id)
+      .where("(published #{op} :pubdate and id <> :id) or (published = :pubdate and id #{op} :id)",
+             pubdate: published, id: id)
+  end
+
+  def hash_permalink
+    Itemhash.insert(permalink) if permalink.present? && unreads == 0
+  end
+
+  def filter_content
+    Shreds::Feed::Filters.apply self
+  end
+
 end
 
 # == Schema Information
