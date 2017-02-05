6ca1274efeb05b2adc200a236b2b87425f2df6b9
Nurahmadie <nurahmadie@gmail.com>
2017-02-06 01:25:01 +0700

    I don't even :<

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -15,9 +15,11 @@ class ArticleSerializer < ApplicationSerializer
   def summary
     text = object.summary || object.content
     nodes = Nokogiri::HTML::DocumentFragment.parse(text.to_s[0..512])
+
+    # FIXME: This is somewhat nonsensical way to cleanup trimmed html for summary text
     if nodes.children.length > 1
       nodes.children = nodes.children[0..-2]
-    elsif nodes.children.first.children.length > 1
+    elsif nodes.children.first && nodes.children.first.children.length > 1
       nodes.children.first.children = nodes.children.first.children[0..-2]
     end
     nodes.to_html
