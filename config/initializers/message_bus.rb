d06156b3fd8043bf23ef4d5ac60cd7a92466cbce
Nurahmadie <nurahmadie@gmail.com>
2017-02-19 16:46:31 +0700

    Add handler for user_id_lookup

--- config/initializers/message_bus.rb
+++ config/initializers/message_bus.rb
@@ -1,7 +1,13 @@
 require 'message_bus'
+require 'lib/shreds/authentication'
 
 if Rails.env.test?
   MessageBus.configure backend: :memory
 else
   MessageBus.redis_config = $redis_config.merge(db: ENV.fetch('redis_db', '15').to_i)
+end
+
+MessageBus.user_id_lookup do |env|
+  req = Rack::Request.new(env)
+  req.session[Shreds::Auth::USER_TOKEN] if req.session
 end
