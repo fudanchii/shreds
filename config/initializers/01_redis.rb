16f156585eb075bb3b80656fc931b9b0a567c5da
Nurahmadie <nurahmadie@gmail.com>
2017-04-01 21:25:26 +0700

    Set default server for redis

--- config/initializers/01_redis.rb
+++ config/initializers/01_redis.rb
@@ -1,5 +1,7 @@
 require 'redis-namespace'
 
+redis_default = '127.0.0.1:6379'
+
 redis_conns = ENV['redis_conns'].to_i <= 0 ?
                 10 :
                 ENV['redis_conns'].to_i
@@ -9,12 +11,12 @@ redis_timeout = ENV['redis_timeout'].to_i <= 0 ?
                   ENV['redis_timeout'].to_i
 
 redis_host = ENV.fetch('redis_host') do
-  ENV.fetch('cache_servers').split(',').first
+  ENV.fetch('cache_servers', redis_default).split(',').first
      .split(':').first
 end
 
 redis_port = ENV.fetch('redis_port') do
-  ENV.fetch('cache_servers').split(',').first
+  ENV.fetch('cache_servers', redis_default).split(',').first
      .split(':').last.to_i || 6379
 end
 
