d3d4f6abdfa316428d3c6d759e2dae90b2ec0755
Nurahmadie <nurahmadie@gmail.com>
2017-04-01 21:33:38 +0700

    A note to never call model class inside migration!!!

--- db/migrate/20130915064117_change_published_should_not_nil.rb
+++ db/migrate/20130915064117_change_published_should_not_nil.rb
@@ -1,6 +1,10 @@
 class ChangePublishedShouldNotNil < ActiveRecord::Migration
   def change
-    Newsitem.where(:published => nil).update_all(:published => DateTime.now)
+    begin
+      Newsitem.where(:published => nil).update_all(:published => DateTime.now)
+    rescue
+      # pass
+    end
     execute "ALTER TABLE newsitems ALTER COLUMN published SET DEFAULT now()"
     execute "ALTER TABLE newsitems ALTER COLUMN published SET NOT NULL"
   end
