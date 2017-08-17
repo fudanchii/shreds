95124b3c87fc7f1059165ac6526d4dfcc191bd68
Nurahmadie <nurahmadie@gmail.com>
2017-08-17 17:10:10 +0700

    Fix redis stuck on stalled connection in openBSD
    - the usage is already wrong since the beginning ;_;

--- config/initializers/01_redis.rb
+++ config/initializers/01_redis.rb
@@ -2,37 +2,35 @@
 
 require 'redis-namespace'
 
-redis_default = '127.0.0.1:6379'
+redis_default = ['127.0.0.1:6379']
 
-redis_conns = if ENV['redis_conns'].to_i <= 0
+redis_conns = if Settings.redis.conns.to_i <= 0
                 10
               else
-                ENV['redis_conns'].to_i
+                Settings.redis.conns.to_i
               end
 
-redis_timeout = if ENV['redis_timeout'].to_i <= 0
+redis_timeout = if Settings.redis.timeout.to_i <= 0
                   30
                 else
-                  ENV['redis_timeout'].to_i
+                  Settings.redis.timeout.to_i
                 end
 
-redis_host = ENV.fetch('redis_host') do
-  ENV.fetch('cache_servers', redis_default).split(',').first
+redis_host = Settings.redis.host ||
+  (Settings.cache.servers || redis_default).first
      .split(':').first
-end
 
-redis_port = ENV.fetch('redis_port') do
-  ENV.fetch('cache_servers', redis_default).split(',').first
+redis_port = Settings.redis.port ||
+  (Settings.cache.servers || redis_default).first
      .split(':').last.to_i || 6379
-end
 
 $redis_config = {
   driver: :hiredis,
   host: redis_host,
   port: redis_port,
-  password: ENV['redis_password']
+  password: Settings.redis.password
 }
 
 $redis_pool = ConnectionPool.new(size: redis_conns, timeout: redis_timeout) do
-  Redis::Namespace.new 'shreds', $redis_config
+  Redis::Namespace.new 'shreds', redis: Redis.new($redis_config)
 end
